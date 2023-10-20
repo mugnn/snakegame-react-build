67a661b11961e46ca0c433c11a81bba6011231aa
gusmaooleo <lgcgusmao@gmail.com>
2023-10-20 15:14:40 -0300

    saves

---
+++ src/pages/login-page/index.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import img from './assets/title.png'
+
+const Login = () => {
+  return(
+    <div id='login-page'>
+      <img alt='logo' src={img} id='logo'/>
+      <div id='box'>
+        <div id='validation-box'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/main-page/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+const Main = () => {
+  return(
+    <div id='main-page'>
+    </div>
+  )
+}
+
+export default Main
