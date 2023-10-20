8ca86c525690997443a58193349aa087852211e1
gusmaooleo <lgcgusmao@gmail.com>
2023-10-20 15:12:47 -0300

    saves

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route, /*Link*/ } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Login from './pages/login-page';
+import Main from './pages/main-page';
 import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+
+const App = () => {
+  return (
+  <BrowserRouter>
+    {/* <Link to='/'>page</Link> */}
+    {/* <Link to='/about'>file</Link> */}
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/main' element={<Main />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
